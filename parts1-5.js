a57a5698defad4884bf94685cc4a736a9af870f1
Chels Noel <chelseybevel@Chelseys-MacBook-Air.local>
2020-05-19 18:21:53 -0400

    adding statement

--- parts1-5.js
+++ parts1-5.js
@@ -13,8 +13,15 @@ var daysToMars = hoursToMars / 24;
 // Code an output statement here (use a template literal):
 console.log(SpacecraftName + " would take " + daysToMars + " days to get to Mars.");
 // Part 3: Create a Function ("getDaysToLocation")
+function getDaysToLocation(kilometersAway) {
+    var milesAway = kilometersAway * milesPerKilometer;
+    var hoursToLocation = milesAway / speedMph;
+    var daysToLocation = hoursToLocation / 24;
+    return daysToLocation;
+}
 // Move your output statement from part 2 here. Update the template literal to call
 // the function and print the outputs for a Mars trip and a moon trip.
+console.log(SpacecraftName + " would take " + daysToLocation + " days to get to Mars.");
 // Part 4: Create a Spacecraft Class
 // Create an instance of the class here:
 // Move your output statements from part 3 here. Update the template literals use the
--- parts1-5.ts
+++ parts1-5.ts
@@ -32,6 +32,7 @@ function getDaysToLocation(kilometersAway: number): Number {
 // Move your output statement from part 2 here. Update the template literal to call
 // the function and print the outputs for a Mars trip and a moon trip.
 
+console.log(`${SpacecraftName} would take ${daysToLocation} days to get to Mars.`);
 
 
 
